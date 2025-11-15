f809d4d4c98afd8d920bdccb922aed0b84a0660b
RafikiRick <eldunarirafiki@protonmail.com>
2025-11-15 16:14:48 +0200

    Bug - Fixed Login Redirect

---
+++ app/Http/Responses/LoginResponse.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Responses;
+
+use Illuminate\Http\Request;
+use Laravel\Fortify\Contracts\LoginResponse as LoginResponseContract;
+
+class LoginResponse implements LoginResponseContract
+{
+    /**
+     * Create an HTTP response that represents the object.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function toResponse($request)
+    {
+        // Check if the user is an admin and redirect accordingly
+        $home = auth()->user()->is_admin ? '/admin/dashboard' : '/dashboard';
+
+        return redirect()->intended($home);
+    }
+}
--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use App\Actions\Fortify\CreateNewUser;
 use App\Actions\Fortify\ResetUserPassword;
+use App\Http\Responses\LoginResponse;
+use App\Models\User;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
@@ -12,6 +14,8 @@ use Illuminate\Support\Str;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
 use Laravel\Fortify\Fortify;
+use Laravel\Fortify\Contracts\LoginResponse as LoginResponseContract;
+use Laravel\Fortify\Contracts\TwoFactorLoginResponse as TwoFactorLoginResponseContract;
 
 class FortifyServiceProvider extends ServiceProvider
 {
@@ -31,6 +35,12 @@ class FortifyServiceProvider extends ServiceProvider
         $this->configureActions();
         $this->configureViews();
         $this->configureRateLimiting();
+
+
+        // Register the custom login response
+        $this->app->singleton(LoginResponseContract::class, LoginResponse::class);
+        // Also handle two-factor authentication redirects
+        $this->app->singleton(TwoFactorLoginResponseContract::class, LoginResponse::class);
     }
 
     /**
